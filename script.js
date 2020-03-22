e8c01bb92dc096d9a8b3fbc9272aa134de710fa8
Chanvin Xiao <chanvinxiao@163.com>
2020-03-22 20:54:39 +0800

    winner

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 let nextLabel = '下一个选手：'
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 Vue.component('Square', {
   props: ['value'],
   methods: {
@@ -30,12 +50,21 @@ Vue.component('Board', {
   methods: {
     handleClick(i) {
       const squares = this.squares.slice();
+      if (calculateWinner(squares)) {
+        alert('胜负已定！');
+        return;
+      }
       if (squares[i]){
-        alert('此位置已被占');
+        alert('此位置已被占!');
         return
       }
       squares[i] = this.xIsNext ? 'X' : 'O';
       this.squares = squares;
+      const winner = calculateWinner(squares);
+      if (winner) {
+        this.status = '获胜者：' + winner;
+        return;
+      }
       this.xIsNext = !this.xIsNext;
       this.status = `${nextLabel}${this.xIsNext ? 'X' : 'O'}`
     }
